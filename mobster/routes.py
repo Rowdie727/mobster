71c707f9f3088d87d78d18bc6f6dc8f146bb971e
Chris Rowden <Crowdendunkin@gmail.com>
2021-10-31 02:13:21 -0400

    Added userpages, display posts in descending order

--- mobster/routes.py
+++ mobster/routes.py
@@ -10,7 +10,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
     return render_template('index.html', posts=posts, title='Home')
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -137,3 +138,11 @@ def delete_post(post_id):
     db.session.commit()
     flash('Post deleted!', 'danger')
     return redirect(url_for('index'))
+
+@app.route("/user/<string:username>")
+def user_pages(username):
+    # Display posts by user
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
+    return render_template('user_pages.html', posts=posts, title=username, user=user)
